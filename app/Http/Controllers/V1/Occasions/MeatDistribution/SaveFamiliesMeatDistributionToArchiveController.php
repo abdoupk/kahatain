36737b8aedbe193dc543e501cdd48c6baafa3b22
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-12-09 08:43:44 +0100

    Structuring Meat Distribution
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

---
+++ app/Http/Controllers/V1/Occasions/MeatDistribution/SaveFamiliesMeatDistributionToArchiveController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers\V1\Occasions\MeatDistribution;
+
+use App\Http\Controllers\Controller;
+
+class SaveFamiliesMeatDistributionToArchiveController extends Controller
+{
+    public function __invoke() {}
+}
