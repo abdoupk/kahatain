3d12272c1526d8d83e0b061772aaf40da208c4ad
abdoupk <37216529+abdoupk@users.noreply.github.com>
2024-12-10 13:41:41 +0100

    WIP desktop and mobile views
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

--- resources/js/Pages/Tenant/members/index/theDesktopView.vue
+++ resources/js/Pages/Tenant/members/index/theDesktopView.vue
@@ -1,10 +1,115 @@
 <script lang="ts" setup>
 import type { IndexParams, MembersIndexResource, PaginationData } from '@/types/types'
 
+import BaseTable from '@/Components/Base/table/BaseTable.vue'
+import BaseTbodyTable from '@/Components/Base/table/BaseTbodyTable.vue'
+import BaseTheadTable from '@/Components/Base/table/BaseTheadTable.vue'
+import BaseTrTable from '@/Components/Base/table/BaseTrTable.vue'
+import TheTableTd from '@/Components/Global/DataTable/TheTableTd.vue'
+import TheTableTdActions from '@/Components/Global/DataTable/TheTableTdActions.vue'
+import TheTableTh from '@/Components/Global/DataTable/TheTableTh.vue'
+import SvgLoader from '@/Components/SvgLoader.vue'
+
+import { hasPermission } from '@/utils/helper'
+import { $t } from '@/utils/i18n'
+
 defineProps<{
     members: PaginationData<MembersIndexResource>
     params: IndexParams
 }>()
+
+// eslint-disable-next-line array-element-newline
+const emit = defineEmits(['sort', 'showDeleteModal', 'showEditModal', 'showDetailsModal'])
 </script>
 
-<template></template>
+<template>
+    <div class="intro-y !z-30 col-span-12 hidden overflow-auto @3xl:block lg:overflow-visible">
+        <base-table class="mt-2 border-separate border-spacing-y-[10px]">
+            <base-thead-table>
+                <base-tr-table>
+                    <the-table-th class="text-start"> #</the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.name"
+                        class="text-start"
+                        sortable
+                        @click="emit('sort', 'name')"
+                    >
+                        {{ $t('the_member') }}
+                    </the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.email"
+                        class="text-center"
+                        sortable
+                        @click="emit('sort', 'email')"
+                    >
+                        {{ $t('validation.attributes.email') }}
+                    </the-table-th>
+
+                    <the-table-th class="text-center">{{ $t('validation.attributes.phone') }}</the-table-th>
+
+                    <the-table-th class="text-center">
+                        {{ $t('validation.attributes.zone') }}
+                    </the-table-th>
+
+                    <the-table-th v-if="hasPermission(['update_members', 'delete_members'])" class="text-center">
+                        {{ $t('actions') }}
+                    </the-table-th>
+                </base-tr-table>
+            </base-thead-table>
+
+            <base-tbody-table>
+                <base-tr-table v-for="(member, index) in members.data" :key="member.id" class="intro-x">
+                    <the-table-td class="w-16">
+                        {{ (members.meta.from ?? 0) + index }}
+                    </the-table-td>
+
+                    <the-table-td class="!min-w-40 !max-w-40 truncate">
+                        <a class="font-medium" href="#" @click.prevent="emit('showDetailsModal', member.id)">
+                            {{ member.name }}
+                        </a>
+                    </the-table-td>
+
+                    <the-table-td class="max-w-40 truncate">
+                        {{ member.email }}
+                    </the-table-td>
+
+                    <the-table-td class="text-center">
+                        {{ member.phone }}
+                    </the-table-td>
+
+                    <the-table-td class="w-40">
+                        <div class="whitespace-nowrap text-center">
+                            {{ member.zone?.name }}
+                        </div>
+                    </the-table-td>
+
+                    <the-table-td-actions v-if="hasPermission(['update_members', 'delete_members'])">
+                        <div class="flex items-center justify-center">
+                            <a
+                                v-if="hasPermission('update_members')"
+                                class="me-3 flex items-center"
+                                href="javascript:void(0)"
+                                @click.prevent="emit('showEditModal', member.id)"
+                            >
+                                <svg-loader class="me-1 h-4 w-4 fill-current" name="icon-pen" />
+                                {{ $t('edit') }}
+                            </a>
+
+                            <a
+                                v-if="hasPermission('delete_members')"
+                                class="flex items-center text-danger"
+                                href="javascript:void(0)"
+                                @click="emit('showDeleteModal', member.id)"
+                            >
+                                <svg-loader class="me-1 h-4 w-4 fill-current" name="icon-trash-can" />
+                                {{ $t('delete') }}
+                            </a>
+                        </div>
+                    </the-table-td-actions>
+                </base-tr-table>
+            </base-tbody-table>
+        </base-table>
+    </div>
+</template>
--- resources/js/Pages/Tenant/roles/index/theDesktopView.vue
+++ resources/js/Pages/Tenant/roles/index/theDesktopView.vue
@@ -1,10 +1,119 @@
 <script lang="ts" setup>
 import type { IndexParams, PaginationData, RolesIndexResource } from '@/types/types'
 
+import BaseTable from '@/Components/Base/table/BaseTable.vue'
+import BaseTbodyTable from '@/Components/Base/table/BaseTbodyTable.vue'
+import BaseTheadTable from '@/Components/Base/table/BaseTheadTable.vue'
+import BaseTrTable from '@/Components/Base/table/BaseTrTable.vue'
+import TheTableTd from '@/Components/Global/DataTable/TheTableTd.vue'
+import TheTableTdActions from '@/Components/Global/DataTable/TheTableTdActions.vue'
+import TheTableTh from '@/Components/Global/DataTable/TheTableTh.vue'
+import SvgLoader from '@/Components/SvgLoader.vue'
+
+import { formatDate } from '@/utils/helper'
+
 defineProps<{
     roles: PaginationData<RolesIndexResource>
     params: IndexParams
 }>()
+
+// eslint-disable-next-line
+const emit = defineEmits(['sort', 'showDeleteModal', 'showEditModal'])
 </script>
 
-<template></template>
+<template>
+    <div class="intro-y !z-30 col-span-12 hidden overflow-auto @3xl:block lg:overflow-visible">
+        <base-table class="mt-2 border-separate border-spacing-y-[10px]">
+            <base-thead-table>
+                <base-tr-table>
+                    <the-table-th class="text-start"> #</the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.name"
+                        class="text-start"
+                        sortable
+                        @click="emit('sort', 'name')"
+                    >
+                        {{ $t('the_role') }}
+                    </the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.permissions_count"
+                        class="text-center"
+                        sortable
+                        @click="emit('sort', 'permissions_count')"
+                    >
+                        {{ $t('permissions_count') }}
+                    </the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.users_count"
+                        class="text-center"
+                        sortable
+                        @click="emit('sort', 'users_count')"
+                    >
+                        {{ $t('users_count') }}
+                    </the-table-th>
+
+                    <the-table-th
+                        :direction="params.directions?.created_at"
+                        class="text-center"
+                        sortable
+                        @click="emit('sort', 'created_at')"
+                    >
+                        {{ $t('validation.attributes.created_at') }}
+                    </the-table-th>
+
+                    <the-table-th class="text-center">
+                        {{ $t('actions') }}
+                    </the-table-th>
+                </base-tr-table>
+            </base-thead-table>
+
+            <base-tbody-table>
+                <base-tr-table v-for="(role, index) in roles.data" :key="role.uuid" class="intro-x">
+                    <the-table-td class="w-16">
+                        {{ (roles.meta.from ?? 0) + index }}
+                    </the-table-td>
+
+                    <the-table-td class="!min-w-40 !max-w-40 truncate">
+                        {{ role.name }}
+                    </the-table-td>
+
+                    <the-table-td class="text-center">
+                        {{ role.permissions_count }}
+                    </the-table-td>
+
+                    <the-table-td class="text-center">
+                        {{ role.users_count }}
+                    </the-table-td>
+
+                    <the-table-td class="text-center">
+                        {{ formatDate(role.created_at, 'full') }}
+                    </the-table-td>
+
+                    <the-table-td-actions>
+                        <div class="flex items-center justify-center">
+                            <a
+                                class="me-3 flex items-center"
+                                href="javascript:void(0)"
+                                @click="emit('showEditModal', role.uuid)"
+                            >
+                                <svg-loader class="me-1 h-4 w-4 fill-current" name="icon-pen" />
+                                {{ $t('edit') }}
+                            </a>
+                            <a
+                                class="flex items-center text-danger"
+                                href="javascript:void(0)"
+                                @click="emit('showDeleteModal', role.uuid)"
+                            >
+                                <svg-loader class="me-1 h-4 w-4 fill-current" name="icon-trash-can" />
+                                {{ $t('delete') }}
+                            </a>
+                        </div>
+                    </the-table-td-actions>
+                </base-tr-table>
+            </base-tbody-table>
+        </base-table>
+    </div>
+</template>
