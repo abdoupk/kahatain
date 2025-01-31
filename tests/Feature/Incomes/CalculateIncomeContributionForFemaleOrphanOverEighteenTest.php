349e359f3fb28654668928671116265a4325dad8
abdoupk <37216529+abdoupk@users.noreply.github.com>
2025-01-31 17:31:55 +0100

    Fix issues after depression
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

--- tests/Feature/Incomes/CalculateIncomeContributionForFemaleOrphanOverEighteenTest.php
+++ tests/Feature/Incomes/CalculateIncomeContributionForFemaleOrphanOverEighteenTest.php
@@ -34,7 +34,7 @@ beforeEach(function () {
 
     $this->family = Family::factory()
         ->hasSponsor(1, [
-            'sponsor_type' => SponsorType::WIDOWS_HUSBAND,
+            'sponsor_type' => SponsorType::WIDOWS_HUSBAND->value,
             'tenant_id' => $this->tenant->id,
         ])
         ->hasZone(1, ['tenant_id' => $this->tenant->id])
@@ -48,124 +48,168 @@ beforeEach(function () {
         'family_id' => $this->family->id,
         'tenant_id' => $this->tenant->id,
         'birth_date' => now()->subDays(80),
-        'income' => 1000,
+        'academic_level_id' => null,
+        'income' => 1000.0,
+        'is_handicapped' => false,
+        'gender' => 'female',
+        'family_status' => null,
     ]);
+
+    $this->family->load(['sponsor.orphans']);
 });
 
-it('correctly calculates income contribution for female orphan and college student not complete his studies and family status not selected.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan college student (licence).', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
-        'academic_level_id' => AcademicLevel::wherePhaseKey('university')->inRandomOrder()->first()->id,
-        'gender' => 'female',
-        'is_handicapped' => false,
+        'academic_level_id' => AcademicLevel::wherePhaseKey('licence')->first()->id,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(3000.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(270.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and college student and complete his studies and family status not selected.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan college student (master 1).', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
-        'academic_level_id' => AcademicLevel::wherePhaseKey('university')->whereLevel('متخرج')->first()->id,
-        'gender' => 'female',
-        'is_handicapped' => false,
+        'academic_level_id' => AcademicLevel::whereLevel('الأولى ماستر')->first()->id,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(3000.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(390.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and in university and academic level not selected.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan college student (master 2).', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
-        'family_status' => FamilyStatus::COLLEGE_GIRL->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
+        'academic_level_id' => AcademicLevel::whereLevel('الثانية ماستر')->first()->id,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(200.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(480.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and works.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan college student (doctorate).', function () {
+    $this->family->sponsor->orphans()->first()->update([
+        'birth_date' => now()->subYears(20),
+        'academic_level_id' => AcademicLevel::wherePhaseKey('doctorate')->first()->id,
+    ]);
+
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(2400.0);
+})->group('incomes');
+
+it('correctly calculates income contribution for female orphan college student complete his studies.', function () {
+    $this->family->sponsor->orphans()->first()->update([
+        'birth_date' => now()->subYears(20),
+        'academic_level_id' => AcademicLevel::wherePhaseKey('university')->first()->id,
+    ]);
+
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(3000.0);
+})->group('incomes');
+
+it('correctly calculates income contribution for female orphan is professional.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::PROFESSIONAL_GIRL->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(250.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(250.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and in house and without income.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan is professional the status not set (Vocational training center).', function () {
+    $this->family->sponsor->orphans()->first()->update([
+        'birth_date' => now()->subYears(20),
+        'academic_level_id' => AcademicLevel::wherePhaseKey('vocational_training')->first()->id,
+    ]);
+
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(250.0);
+})->group('incomes');
+
+it('correctly calculates income contribution for female orphan is professional the status not set (Paramedical).', function () {
+    $this->family->sponsor->orphans()->first()->update([
+        'birth_date' => now()->subYears(20),
+        'academic_level_id' => AcademicLevel::wherePhaseKey('paramedical')->first()->id,
+    ]);
+
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(250.0);
+})->group('incomes');
+
+it('correctly calculates income contribution for female orphan at home with no income.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::AT_HOME_WITH_NO_INCOME->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
+        'is_unemployed' => true,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(3000.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))
+        ->toBe(3000.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and in house with grant (منحة البطالة).', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan at home with income.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::AT_HOME_WITH_INCOME->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(300.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(300.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and single.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan single female / employed.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::SINGLE_FEMALE_EMPLOYEE->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(400.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(400.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and married.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan married .', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::MARRIED->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(300.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(300.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and divorced with family.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan divorced with family.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::DIVORCED_WITH_FAMILY->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(1000.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(1000.0);
 })->group('incomes');
 
-it('correctly calculates income contribution for female orphan and divorced without family.', function () {
-    $this->orphan->update([
+it('correctly calculates income contribution for female orphan divorced outside the family.', function () {
+    $this->family->sponsor->orphans()->first()->update([
         'birth_date' => now()->subYears(20),
         'family_status' => FamilyStatus::DIVORCED_OUTSIDE_FAMILY->value,
-        'gender' => 'female',
-        'academic_level_id' => null,
-        'is_handicapped' => false,
     ]);
 
-    expect(calculateOrphanIncomes($this->orphan))->toBe(1000.0);
+    $this->family->load('sponsor.orphans');
+
+    expect(calculateContributionsForFemaleOrphan($this->family->sponsor->orphans->first(), $this->calculation))->toBe(1000.0);
 })->group('incomes');
