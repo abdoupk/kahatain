a0113fb074393230168f6bb0ec91d96fba944ff8
abdoupk <37216529+abdoupk@users.noreply.github.com>
2025-02-11 05:16:26 +0100

    Fix issues after depression
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

--- app/Http/Controllers/V1/API/City/WilayaController.php
+++ app/Http/Controllers/V1/API/City/WilayaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1\API\City;
 
 use App\Http\Controllers\Controller;
 use App\Models\City;
+use Cache;
 use Illuminate\Http\JsonResponse;
 
 class WilayaController extends Controller
@@ -11,12 +12,14 @@ class WilayaController extends Controller
     public function __invoke(): JsonResponse
     {
         return response()->json([
-            'wilayas' => City::select([
-                'wilaya_code',
-                'wilaya_name',
-            ])->orderBy('wilaya_code')
-                ->groupBy(['wilaya_code', 'wilaya_name'])
-                ->get(),
+            'wilayas' => Cache::rememberForever('wilayas', function () {
+                City::select([
+                    'wilaya_code',
+                    'wilaya_name',
+                ])->orderBy('wilaya_code')
+                    ->groupBy(['wilaya_code', 'wilaya_name'])
+                    ->get();
+            }),
         ]);
     }
 }
