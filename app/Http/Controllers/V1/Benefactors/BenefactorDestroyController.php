4fc29c21dd8455eb04d9a0f82c03fe4f8c310295
abdoupk <37216529+abdoupk@users.noreply.github.com>
2025-08-12 18:35:50 +0100

    Fix issues after depression
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

--- app/Http/Controllers/V1/Benefactors/BenefactorDestroyController.php
+++ app/Http/Controllers/V1/Benefactors/BenefactorDestroyController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\V1\Benefactors;
 use App\Http\Controllers\Controller;
 use App\Jobs\V1\Benefactor\BenefactorTrashedJob;
 use App\Models\Benefactor;
+use App\Models\Family;
+use App\Models\Sponsorship;
 use Illuminate\Routing\Controllers\HasMiddleware;
 
 class BenefactorDestroyController extends Controller implements HasMiddleware
@@ -17,19 +19,17 @@ class BenefactorDestroyController extends Controller implements HasMiddleware
     public function __invoke(Benefactor $benefactor)
     {
         $benefactor->sponsorships()->each(function (Sponsorship $sponsorship) {
-            $recipientable= $sponsorship->recipientable();
+            $recipientable = $sponsorship->recipientable()->first();
+
+            $sponsorship->delete();
 
             if ($recipientable instanceof Family) {
                 monthlySponsorship($recipientable);
-            }
-
-            else{
+            } else {
                 monthlySponsorship($recipientable->family);
             }
         });
 
-        $benefactor->sponsorships()->delete();
-        
         $benefactor->delete();
 
         dispatch(new BenefactorTrashedJob($benefactor, auth()->user()));
