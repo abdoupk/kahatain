2fc0ca05bf4b59425c94af484d332ddacc76c383
abdoupk <37216529+abdoupk@users.noreply.github.com>
2025-08-13 17:56:26 +0100

    Fix issues after depression
    Signed-off-by: abdoupk <37216529+abdoupk@users.noreply.github.com>

--- app/Http/Controllers/V1/Orphans/OrphanEditController.php
+++ app/Http/Controllers/V1/Orphans/OrphanEditController.php
@@ -18,6 +18,16 @@ class OrphanEditController extends Controller implements HasMiddleware
 
     public function __invoke(Orphan $orphan): Response
     {
+        ray($orphan->load(
+            'babyNeeds',
+            'creator',
+            'shoesSize',
+            'pantsSize',
+            'shirtSize',
+            'media',
+            'institution'
+        ));
+
         return Inertia::render(
             'Tenant/orphans/edit/OrphanEditPage',
             [
